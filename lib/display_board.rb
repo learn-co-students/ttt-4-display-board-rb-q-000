d2e5c89ab01abf7e4b2de9e1ca3cb1b4041e96cf
eaylward8 <eaylward8@gmail.com>
2015-10-15 15:08:13 -0400

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,23 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = ["", "", "", "", "", "", "", "", ""]
+
+def display_board(board)
+  #go over array items using each_with_index method
+  board.each_with_index do |x, i|
+    #if index is 2 or 5
+    if i == 2 || i == 5
+      #print item with spaces
+      puts " #{x} "
+      #print line
+      puts "-----------"
+    #else if index is 8  
+    elsif i == 8
+      #print item with spaces
+      puts " #{x} "
+    else
+      #else print item with spaces and pipe |
+      print " #{x} |"
+    end #end if statements
+  end #end do
+end #end def
