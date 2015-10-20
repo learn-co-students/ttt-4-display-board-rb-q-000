97a336253a1137bea5768e9c8c0b6bc1209ccb36
antonio ortiz <17antonio.ortiz@gmail.com>
2015-10-20 11:32:31 -0400

    Donerz!

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,17 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
+def display_board(board)
+  cc = board
+  row1 = ' '+ cc[0] + ' ' + '|' + ' ' + cc[1] + ' ' + '|' + ' ' + cc[2] + ' '
+  row2 = ' '+ cc[3] + ' ' + '|' + ' ' + cc[4] + ' ' + '|' + ' ' + cc[5] + ' '
+  row3 = ' '+ cc[6] + ' ' + '|' + ' ' + cc[7] + ' ' + '|' + ' ' + cc[8] + ' '
+  divider = '-----------'
+  puts row1
+  puts divider
+  puts row2
+  puts divider
+  puts row3
+end
+
+display_board(board)
