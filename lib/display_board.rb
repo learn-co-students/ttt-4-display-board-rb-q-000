312176e6a24a9530a1c9ecb93a94a3443d9a25e6
ptcloutier <cloutier.perrin@gmail.com>
2015-10-22 20:00:57 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,9 +5,3 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-display_board(board)
-display_board(board)
-display_board(board)
