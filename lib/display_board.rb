9b1ecb5290374f484896fa5989dfb27b999bc496
Paul Tyng <paul@paultyng.net>
2015-10-23 12:35:48 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board)
+  puts board.each_slice(3).map{ |row| row.map{ |cell| cell.center(3) }.join('|') }.join("\n#{'-' * 11}\n")
+end
