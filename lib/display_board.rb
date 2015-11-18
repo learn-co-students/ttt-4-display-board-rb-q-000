d4ffc42848f384de93bf8b07dbea27e948fbb7b6
lifeiscontent <aaron@lifeiscontent.net>
2015-11-18 13:35:13 -0800

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,17 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board)
+  result = board.map.with_index do |value, index|
+    case index % 3
+    when 0
+      " #{value} "
+    when 1
+      "| #{value} |"
+    when 2
+      " #{value} \n" + (index == (board.length - 1) ? "" : "-----------\n")
+    end
+  end.join('')
+
+  puts result
+end
