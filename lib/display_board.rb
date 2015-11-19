ced608aaa4f840aecf9b40fdcd9889284fda6812
tomsyouruncle <thomas.e.turner@gmail.com>
2015-11-18 19:57:49 -0500

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,19 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def build_line(line)
+  output = ""
+  line.length.times do |i|
+    output << " " + line[i] + " "
+    output << "|" if i < line.length - 1 
+  end
+  output
+end
+
+def display_board(board)
+  3.times do |i|
+    puts build_line(board[3 * i ... 3 * (i+1)])
+    puts '-' * 11 if i < 2
+  end
+
+end
