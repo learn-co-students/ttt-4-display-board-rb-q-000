8741be4665af0af727fbc2897bf25c6f0c1b2906
tjnz <tommyskies@hotmail.com>
2015-11-22 22:00:54 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,14 +4,12 @@ def display_board(board = Array.new(9) {" "})
   bar = "-----------"
   start_inc = 0
   3.times do
-    row = " #{board[start_inc]} | #{board[start_inc+1]} | #{board[start_inc+2]} "
-    puts row
+    puts " #{board[start_inc]} | #{board[start_inc+1]} | #{board[start_inc+2]} "
     start_inc = start_inc + 3
     if start_inc < 9
       puts bar
-    else
-      return
     end
   end
+  return
 end
 
