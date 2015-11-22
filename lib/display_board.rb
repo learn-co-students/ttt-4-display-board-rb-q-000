ae4c4510c639d8d88cba6f6098e49e2464b15920
Morgan LaPeter <parenteamllc@gmail.com>
2015-11-22 13:33:45 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,16 +3,16 @@
 
 
 def display_board(board)
-  top row=" #{board[0]} | #{board[1]} | #{board[2]} "
+  top_row=" #{board[0]} | #{board[1]} | #{board[2]} "
   dashes="-----------"
-  middle row=" #{board[3]} | #{board[4]} | #{board[5]} "
-  bottom row=" #{board[6]} | #{board[7]} | #{board[8]} "
+  middle_row=" #{board[3]} | #{board[4]} | #{board[5]} "
+  bottom_row=" #{board[6]} | #{board[7]} | #{board[8]} "
 
-print top row
+print top_row
 print dashes
-print middle row
+print middle_row
 print dashes
-print bottom row
+print bottom_row
 end
 board = [" "," "," "," "," "," "," "," "," "]
 display_board(board)
