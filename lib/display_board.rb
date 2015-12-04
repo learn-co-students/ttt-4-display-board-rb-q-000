35f5bb8e6e5c1cc72491dd8709c35851868ae883
marcelgalang <marcelgalang@gmail.com>
2015-12-04 18:39:34 -0500

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  puts "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   " if board == [" "," "," "," "," "," "," "," "," "]
-else
+#   puts "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   " if board == [" "," "," "," "," "," "," "," "," "]
+# else
   pipe = "|"
   border = "-----------"
   puts " #{board[0]} #{pipe} #{board[1]} #{pipe} #{board[2]} "
