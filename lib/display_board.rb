9ab302c1e4239f47d7eec05748baf05ae67c6379
Anna Kiefer <annaskiefer@gmail.com>
2015-12-05 22:42:03 +0000

    nitrous-backup at Sat Dec  5 22:42:02 UTC 2015

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,7 @@
-def display_board(board)
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+position = ["X", "O"]
+
+def display_board(board, position)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
