4697745858fea9485ea7a8a9b7ac1ae2544c35a6
zevav <zev@averba.ch>
2015-12-07 17:59:30 -0500

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+#
+def display_board(board)
+  hor = "-----------\n"
+  pipe = " | "
+  puts " #{board[0]}#{pipe}#{board[1]}#{pipe}#{board[2]} \n#{hor} #{board[3]}#{pipe}#{board[4]}#{pipe}#{board[5]} \n#{hor} #{board[6]}#{pipe}#{board[7]}#{pipe}#{board[8]} \n"
+end
