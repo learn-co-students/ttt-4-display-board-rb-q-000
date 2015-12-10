ae9588232e12a006dc35a059e4e149143866849e
Jurtly <jurtly@icloud.com>
2015-12-10 11:20:41 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,28 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+#empty board
+def display_board(board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "])
+puts "#{board[0]}  |#{board[1]}  |#{board[2]}  "
+  puts "-----------"
+puts "#{board[3]}  |#{board[4]}  |#{board[5]}  "
+  puts "-----------"
+puts "#{board[6]}  |#{board[7]}  |#{board[8]}  "
+end
+#board with X in the middle
+def display_board(board = ["   ", "   ", "   ", "   ", " X ", "   ", "   ", "   ", "   "])
+
+puts "#{board[0]}  |  #{board[1]}|  #{board[2]}"
+  puts "-----------"
+puts "#{board[3]}  | #{board[4]} |  #{board[5]}"
+  puts "-----------"
+puts "#{board[6]}  |  #{board[7]}|  #{board[8]}"
+end
+#board with O in the top left
+def display_board(board = [" O ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "])
+
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
---
+++ lib/display_board3.rb
@@ -0,0 +1,106 @@
+# Define display_board that accepts a board and prints
+# out the current state.
+
+#board X1 - lose
+def display_board(board = [" O ", " X ", "   ", "   ", " X ", "   ", " X ", " O ", "   "])
+  puts "X1"
+puts "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts "-----------"
+puts "#{board[3]}|#{board[4]}|#{board[5]}"
+  puts "-----------"
+puts "#{board[6]}|#{board[7]}|#{board[8]}"
+end
+
+display_board()
+
+
+#board X2 empty
+def display_board(board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "])
+ puts "X2"
+puts "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts "-----------"
+puts "#{board[3]}|#{board[4]}|#{board[5]}"
+  puts "-----------"
+puts "#{board[6]}|#{board[7]}|#{board[8]}"
+end
+
+display_board()
+
+def display_board(board = ["   ", "   ", "   ", "   ", " X ", "   ", "   ", "   ", "   "])
+  puts "X3"
+puts "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts "-----------"
+puts "#{board[3]}|#{board[4]}|#{board[5]}"
+  puts "-----------"
+puts "#{board[6]}|#{board[7]}|#{board[8]}"
+end
+
+display_board()
+
+def display_board(board = [" O ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "])
+  puts "X4"
+puts "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts "-----------"
+puts "#{board[3]}|#{board[4]}|#{board[5]}"
+  puts "-----------"
+puts "#{board[6]}|#{board[7]}|#{board[8]}"
+end
+
+display_board()
+
+def display_board(board = [" O ", "   ", "   ", "   ", " X ", "   ", "   ", "   ", "   "])
+  puts "X5"
+puts "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts "-----------"
+puts "#{board[3]}|#{board[4]}|#{board[5]}"
+  puts "-----------"
+puts "#{board[6]}|#{board[7]}|#{board[8]}"
+end
+
+display_board()
+
+def display_board(board = [" X ", " X ", " X ", "   ", "   ", "   ", "   ", "   ", "   "])
+  puts "X6"
+puts "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts "-----------"
+puts "#{board[3]}|#{board[4]}|#{board[5]}"
+  puts "-----------"
+puts "#{board[6]}|#{board[7]}|#{board[8]}"
+end
+
+display_board()
+
+
+def display_board(board = ["   ", "   ", "   ", "   ", "   ", "   ", " O ", " O ", " O "])
+  puts "X7"
+puts "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts "-----------"
+puts "#{board[3]}|#{board[4]}|#{board[5]}"
+  puts "-----------"
+puts "#{board[6]}|#{board[7]}|#{board[8]}"
+end
+
+display_board()
+
+def display_board(board = [" X ", "   ", "   ", "   ", " X ", "   ", "   ", "   ", " X "])
+  puts "X8"
+puts "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts "-----------"
+puts "#{board[3]}|#{board[4]}|#{board[5]}"
+  puts "-----------"
+puts "#{board[6]}|#{board[7]}|#{board[8]}"
+end
+
+display_board()
+
+def display_board(board = ["   ", "   ", " O ", "   ", " O ", "   ", " O ", "   ", "   "])
+  puts "X9"
+puts "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts "-----------"
+puts "#{board[3]}|#{board[4]}|#{board[5]}"
+  puts "-----------"
+puts "#{board[6]}|#{board[7]}|#{board[8]}"
+end
+
+display_board()
+
