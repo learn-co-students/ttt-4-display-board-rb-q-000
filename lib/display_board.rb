1b3d97ac2ddcdd62dfb8fd3b655e1ba67438ac1b
Justin-Mullins <justin.mullins009@gmail.com>
2015-12-19 15:24:00 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,17 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board(board)
+  dash = "-----------"
+  line1 = " #{board[0]} | #{board[1]} | #{board[2]} "
+  line2 = " #{board[3]} | #{board[4]} | #{board[5]} "
+  line3 = " #{board[6]} | #{board[7]} | #{board[8]} "
+
+  puts "#{line1}"
+  puts "#{dash}"
+  puts "#{line2}"
+  puts "#{dash}"
+  puts "#{line3}"
+end
+#board = [" "," "," "," "," "," "," "," "," "]
+#display_board(board)
