c6540deb4426120ff286edf588df16433fa4b2d8
sigkapphi <sigkapphi@gmail.com>
2015-12-22 09:33:29 +0000

    Done.

---
+++ display_boardprac.rb
@@ -0,0 +1,161 @@
+boardempty = [ " "," "," "," "," "," ", " ", " "," "]
+board1 = [ " "," "," "," ","X"," ", " ", " "," "]
+board2 = [ "O"," "," "," "," "," ", " ", " "," "]
+board3 = [ "O"," "," "," ","X"," ", " ", " "," "]
+board4 = [ "X","X","X"," "," "," ", " ", " "," "]
+board5 = [ " "," "," "," "," "," ", "O", "O","O"]
+board6 = [ "X"," "," "," ","X"," ", " ", " ","X"]
+board7 = [ " "," ","O"," ","O"," ", "O", " "," "]
+board8 = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
+board9 = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+
+board = [ " "," "," "," "," "," ", " ", " "," "]
+BoardA= " #{board[0]} | #{board[1]} | #{board[2]} "
+BoardB= "-----------"
+BoardC= " #{board[3]} | #{board[4]} | #{board[5]} "
+BoardD= " #{board[6]} | #{board[7]} | #{board[8]} "
+
+def display_board(board)
+  puts "#{BoardA}"
+  puts "#{BoardB}"
+  puts "#{BoardC}"
+  puts "#{BoardB}"
+  puts "#{BoardD}"
+end
+display_board(board)
+
+board= [ " "," "," "," ","X"," ", " ", " "," "]
+BoardA= " #{board[0]} | #{board[1]} | #{board[2]} "
+BoardB= "-----------"
+BoardC= " #{board[3]} | #{board[4]} | #{board[5]} "
+BoardD= " #{board[6]} | #{board[7]} | #{board[8]} "
+
+def display_board(board)
+  puts "#{BoardA}"
+  puts "#{BoardB}"
+  puts "#{BoardC}"
+  puts "#{BoardB}"
+  puts "#{BoardD}"
+end
+display_board(board)
+
+board= [ "O"," "," "," "," "," ", " ", " "," "]
+BoardA= " #{board[0]} | #{board[1]} | #{board[2]} "
+BoardB= "-----------"
+BoardC= " #{board[3]} | #{board[4]} | #{board[5]} "
+BoardD= " #{board[6]} | #{board[7]} | #{board[8]} "
+
+def display_board(board)
+  puts "#{BoardA}"
+  puts "#{BoardB}"
+  puts "#{BoardC}"
+  puts "#{BoardB}"
+  puts "#{BoardD}"
+end
+display_board(board)
+
+board= [ "O"," "," "," ","X"," ", " ", " "," "]
+BoardA= " #{board[0]} | #{board[1]} | #{board[2]} "
+BoardB= "-----------"
+BoardC= " #{board[3]} | #{board[4]} | #{board[5]} "
+BoardD= " #{board[6]} | #{board[7]} | #{board[8]} "
+
+def display_board(board)
+  puts "#{BoardA}"
+  puts "#{BoardB}"
+  puts "#{BoardC}"
+  puts "#{BoardB}"
+  puts "#{BoardD}"
+end
+display_board(board)
+
+board= [ "X","X","X"," "," "," ", " ", " "," "]
+BoardA= " #{board[0]} | #{board[1]} | #{board[2]} "
+BoardB= "-----------"
+BoardC= " #{board[3]} | #{board[4]} | #{board[5]} "
+BoardD= " #{board[6]} | #{board[7]} | #{board[8]} "
+
+def display_board(board)
+  puts "#{BoardA}"
+  puts "#{BoardB}"
+  puts "#{BoardC}"
+  puts "#{BoardB}"
+  puts "#{BoardD}"
+end
+display_board(board)
+
+board= [ " "," "," "," "," "," ", "O", "O","O"]
+BoardA= " #{board[0]} | #{board[1]} | #{board[2]} "
+BoardB= "-----------"
+BoardC= " #{board[3]} | #{board[4]} | #{board[5]} "
+BoardD= " #{board[6]} | #{board[7]} | #{board[8]} "
+
+def display_board(board)
+  puts "#{BoardA}"
+  puts "#{BoardB}"
+  puts "#{BoardC}"
+  puts "#{BoardB}"
+  puts "#{BoardD}"
+end
+display_board(board)
+
+board= [ "X"," "," "," ","X"," ", " ", " ","X"]
+BoardA= " #{board[0]} | #{board[1]} | #{board[2]} "
+BoardB= "-----------"
+BoardC= " #{board[3]} | #{board[4]} | #{board[5]} "
+BoardD= " #{board[6]} | #{board[7]} | #{board[8]} "
+
+def display_board(board)
+  puts "#{BoardA}"
+  puts "#{BoardB}"
+  puts "#{BoardC}"
+  puts "#{BoardB}"
+  puts "#{BoardD}"
+end
+display_board(board)
+
+board= [ " "," ","O"," ","O"," ", "O", " "," "]
+BoardA= " #{board[0]} | #{board[1]} | #{board[2]} "
+BoardB= "-----------"
+BoardC= " #{board[3]} | #{board[4]} | #{board[5]} "
+BoardD= " #{board[6]} | #{board[7]} | #{board[8]} "
+
+def display_board(board)
+  puts "#{BoardA}"
+  puts "#{BoardB}"
+  puts "#{BoardC}"
+  puts "#{BoardB}"
+  puts "#{BoardD}"
+end
+display_board(board)
+
+board= ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
+BoardA= " #{board[0]} | #{board[1]} | #{board[2]} "
+BoardB= "-----------"
+BoardC= " #{board[3]} | #{board[4]} | #{board[5]} "
+BoardD= " #{board[6]} | #{board[7]} | #{board[8]} "
+
+def display_board(board)
+  puts "#{BoardA}"
+  puts "#{BoardB}"
+  puts "#{BoardC}"
+  puts "#{BoardB}"
+  puts "#{BoardD}"
+end
+display_board(board)
+
+board= ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+BoardA= " #{board[0]} | #{board[1]} | #{board[2]} "
+BoardB= "-----------"
+BoardC= " #{board[3]} | #{board[4]} | #{board[5]} "
+BoardD= " #{board[6]} | #{board[7]} | #{board[8]} "
+
+def display_board(board)
+  puts "#{BoardA}"
+  puts "#{BoardB}"
+  puts "#{BoardC}"
+  puts "#{BoardB}"
+  puts "#{BoardD}"
+end
+display_board(board)
+
