3c32cf4b921426abacf1e76a9593080e8023c87b
Nicholas-Maxwell <nick.maxwell18@gmail.com>
2016-01-11 21:26:31 -0500

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,105 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+board = ["O", "X", "X", "X", "X", "X", "O", "O", "X", "X", "O", "O", "O", "X", "X" ]
+def display_board (board)
+  
+puts "O: #{board[0]}, X: #{board[1]} X: #{board[2]},X: #{board[3]}, X: #{board[4]}, X: #{board[5]}, O: #{board[6]}, O: #{board[7]}, X: #{board[8]}, O: #{board[9]}, O: #{board[10]}, O: #{board[11]}, X: #{board[12]}, X: #{board[13]}"
+
+puts    "   |   |   "
+
+puts   "-----------"
+
+puts    "   |   |   "
+
+puts   "-----------" 
+
+puts    "   |   |   "
+
+puts    "   |   |   "
+
+puts   "-----------"
+
+puts    "   | X |   "
+
+puts   "-----------" 
+
+puts    "   |   |   "
+
+puts    " O |   |   "
+
+puts   "-----------"
+
+puts    "   |   |   "
+
+puts   "-----------" 
+
+puts    "   |   |   "
+
+puts    " X | X | X "
+
+puts   "-----------"
+
+puts    "   |   |   "
+
+puts   "-----------" 
+
+puts    "   |   |   "
+
+puts    "   |   |   "
+
+puts   "-----------"
+
+puts    "   |   |   "
+
+puts   "-----------" 
+
+puts    " O | O | O "
+
+puts    " X |   |   "
+
+puts   "-----------"
+
+puts    "   | X |   "
+
+puts   "-----------" 
+
+puts    "   |   | X "
+
+puts    "   |   | O "
+
+puts   "-----------"
+
+puts    "   | O |   "
+
+puts   "-----------" 
+
+puts    " O |   |   "
+
+puts    " X | O | O "
+
+puts   "-----------"
+
+puts    " X | O | X "
+
+puts   "-----------" 
+
+puts    " O | X | X "
+
+puts    " X | O | O "
+
+puts   "-----------"
+
+puts    " O | O | O "
+
+puts   "-----------" 
+
+puts    " O | O | X "
+
+puts    " O | X | O "
+
+puts   "-----------"
+
+puts    " X | O | X "
+
+puts   "-----------" 
+
+puts    " O | O | X "
+end 
