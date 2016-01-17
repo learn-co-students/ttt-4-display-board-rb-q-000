6c72bf8393819619949ddd8978efdfb37adf41d4
dan-scott-ting <daniel.scott.ting@gmail.com>
2016-01-17 09:16:00 +0000

    nitrous-backup at Sun Jan 17 09:16:00 UTC 2016

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(board = [" "," "," "," ","X"," "," "," "," "])
+def display_board(board = [" "," "," "," "," "," "," "," "," "])
   row_seperator = "-----------"
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts row_seperator
