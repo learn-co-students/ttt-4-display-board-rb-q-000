36d7d3ed09db9b2e8f6dd510038a4e93ee715501
jbok4 <jbok4@aol.com>
2016-01-25 18:14:50 -0500

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,26 @@
 # Define display_board that accepts a board and prints
-# out the current state.
+  # out the current state.
+ board = [" "," "," "," "," "," "," "," "," "]
+ 
+ def display_board(board)
+   puts "   ""|""   ""|""   "
+   puts "-----------"
+   puts "   ""|""   ""|""   "
+   puts "-----------"
+   puts "   ""|""   ""|""   "
+ 
+ end
+ 
+ def board_insert(position, value)
+   position = " "
+   print "Please select a square by entering 1-9, 1 for the top left and 9 for the bottom right"
+   position = gets.to_i
+   value = " "
+   print "Are you X or O:"
+   value = gets.chomp
+   board["#{position}" - 1] = [value]
+ 
+ end
+ 
+ display_board(board)
+ #board_insert(position, value) 
