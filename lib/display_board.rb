be704874a10dbf89d00bd5d4864a81eb382bab23
zaynrahn <zaynrahn@gmail.com>
2016-02-04 18:10:39 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,15 +1,5 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = [" "," "," "," "," "," "," "," "," "]
-board = [" "," "," "," ","X"," "," "," "," "]
-board = ["O"," "," "," "," "," "," "," "," "]
-board = ["O"," "," "," ","X"," "," "," "," "]
-board = ["X","X","X"," "," "," "," "," "," "]
-board = [" "," "," "," "," "," ","O","O","O"]
-board = ["X"," "," "," ","X"," "," "," ","X"]
-board = [" "," ","O"," ","O"," ","O"," "," "]
-board = ["X","X","X","X","X","X","X","X","X"]
-board = ["O","O","O","O","O","O","O","O","O"]
 def display_board(board)
   print " #{board[0]} | #{board[1]} | #{board[2]} "
   print "-----------"
