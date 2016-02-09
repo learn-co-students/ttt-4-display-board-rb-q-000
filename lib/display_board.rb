d4816e669608145956a3f37bf9d88e96035c60d1
cmendez91 <cmendez1991@gmail.com>
2016-02-09 17:43:32 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,27 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+board = [" "," "," "," "," "," "," "," "," "]
+def display_board(board)
+  puts ("   |   |   ")
+  puts ("-----------")
+  puts ("   |   |   ")
+  puts ("-----------")
+  puts ("   |   |   ")
+end
+
+board = [" "," "," "," ","X"," "," "," "," "]
+def display_board (board)
+  pipe = "|"
+dash = "-----------"
+  puts(" #{board[0]} #{pipe} #{board[1]} #{pipe} #{board[2]} ")
+  puts(dash)
+  puts(" #{board[3]} #{pipe} #{board[4]} #{pipe} #{board[5]} ")
+  puts(dash)
+  puts(" #{board[6]} #{pipe} #{board[7]} #{pipe} #{board[8]} ")
+end
+
+
+
+
+
+
+
+
