b68133c1b575c9aac5190709c42140cbf71dd9ce
vandepool <contact.vandepool@gmail.com>
2016-02-12 19:55:41 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,5 +8,5 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-board = ["X", "", "", "", "X", "", "", "", "X"]
+board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
 display_board(board)
