0495a09e380b2bae8d614f957518446f971db232
robert-newcitizens <robert@newcitizens.com>
2016-02-18 10:38:18 -0700

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,65 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+
+  puts "   |   |   "
+  puts "-----------"
+  puts "   | X |   "
+  puts "-----------"
+  puts "   |   |   "
+
+  puts " O |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+
+  puts " O |   |   "
+  puts "-----------"
+  puts "   | X |   "
+  puts "-----------"
+  puts "   |   |   "
+
+  puts " X | X | X "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts " O | O | O "
+
+  puts " X |   |   "
+  puts "-----------"
+  puts "   | X |   "
+  puts "-----------"
+  puts "   |   | X "
+
+  puts "   |   | O "
+  puts "-----------"
+  puts "   | O |   "
+  puts "-----------"
+  puts " O |   |   "
+
+  puts " X | O | O "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+
+  puts " X | O | X "
+  puts "-----------"
+  puts " O | X | X "
+  puts "-----------"
+  puts " O | X | O "
+
+  puts "#{board[0]}, #{board[1]}, #{board[2]}, #{board[3]}, #{board[4]}, #{board[5]}, #{board[6]}, #{board[7]}, #{board[8]}"
+end
