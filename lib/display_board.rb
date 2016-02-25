e046a4b8156f851a182b88775a0bbd6c1f0407d2
blessings008 <qualityworks360@gmail.com>
2016-02-25 03:18:33 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,73 +1,7 @@
 def display_board(board)
-  board = ["   ", "|", "   ", "|", "   ", "   ", "|", "   ", "|", "   ", "   ", |", "   ", "|", "   "]
-  puts "#{board[0]}, "|", #{board[1]}, "|", #{board[2]}"
-  puts "___________"
-  puts "#{board[3]}, "|", "#{board[4]}, "|", #{board[5]}
-  puts "___________"
-  puts #{board[6]}, "|", #{board[7]}, "|", #{board[8]}
- end
-  board = ["   ", "|", "   ", "|", "   ", "   ", "|", " X ", "|", "   ", "   ", "|", "   ", "|", "   "]
-  puts "#{board[0]}, "|", #{board[1]}, "|", #{board[2]}"
-  puts "___________"
-  puts "#{board[3]}, "|", "#{board[4]}, "|", #{board[5]}
-  puts "___________"
-  puts #{board[6]}, "|", #{board[7]}, "|", #{board[8]}
- end
-  board = [" O ", "|", "   ", "|", "   ", "   ", "|", "   ", "|", "   ", "   ", "|", "   ", "|", "   "]
-  board[0] = "0"
-  puts "#{board[0]}, "|", #{board[1]}, "|", #{board[2]}"
-  puts "___________"
-  puts "#{board[3]}, "|", "#{board[4]}, "|", #{board[5]}
-  puts "___________"
-  puts #{board[6]}, "|", #{board[7]}, "|", #{board[8]}
- end
-  board = [" O ", "|", "   ", "|", "   ", "   ", "|", " X ", "|", "   ", "   ", "|", "   ", "|", "   "]
-  board[0] = "O"
-  board[4] = "X"
-  puts "#{board[0]}, "|", #{board[1]}, "|", #{board[2]}"
-  puts "___________"
-  puts "#{board[3]}, "|", "#{board[4]}, "|", #{board[5]}
-  puts "___________"
-  puts #{board[6]}, "|", #{board[7]}, "|", #{board[8]}
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-  board = [" X ", "|", " X ", "|", " X ", "   ", "|", "   ", "|", "   ", "   ", "|", "   ", "|", "   "]
-  board[0] = "X"
-  board[1] = "X"
-  board[2] = "X"
-puts "#{board[0]}, "|", #{board[1]}, "|", #{board[2]}"
-  puts "___________"
-  puts "#{board[3]}, "|", "#{board[4]}, "|", #{board[5]}
-  puts "___________"
-  puts #{board[6]}, "|", #{board[7]}, "|", #{board[8]}
-end
-  board = ["  ", "|", "   ", "|", "  ", "   ", "|", "  ", "|", "  ", " O ", "|", " O ", "|", " O "]
-  board[6] = "O"
-  board[7] = "O"
-  board[8] = "O"
- end
-  board = [" X ", "|", "  ", "|", "  ", "  ", "|", " X ", "|", "  ", "  ", "|", " X ", "|", "  "]
-  board[0] = "X"
-  board[4] = "X"
-  board[8] = "X"
- end
-  board = [" X ", "|", " X ", "|", " X ", " X ", "|", " X ", "|", " X ", " X ", "|", " X ", "|", " X "]
-  board[0] = "X"
-  board[1] = "X"
-  board[2] = "X"
-  board[3] = "X"
-  board[4] = "X"
-  board[5] = "X"
-  board[6] = "X"
-  board[7] = "X"
-  board[8] = "X"
- end
-
-
-
-
-
-
-
-
-
-
