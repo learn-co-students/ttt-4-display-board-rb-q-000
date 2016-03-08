6df20cb3b8810e4c250e47965a8cec915ea27d81
Vannak Chhay <vannak.chhay1@gmail.com>
2016-03-08 16:32:43 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,10 +2,10 @@
 # out the current state.
 def display_board(board)
   #prints current state
-  print " #{board[0]} | #{board[1]} | #{board[2]} "
-  print "\n-----------\n"
-  print " #{board[3]} | #{board[4]} | #{board[5]} "
-  print "\n-----------\n"
-  print " #{board[6]} | #{board[7]} | #{board[8]} \n"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 
 end
