01c1d78353ea9808d53d1d2cd76645224647264f
LizSmall <liz_too@hotmail.com>
2016-03-28 21:19:53 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,3 +2,76 @@
 # out the current state.
 
 board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+
+print board = [" "," "," ","X"," "," "," "," "," "]
+
+  puts "   |   |   "
+  puts "-----------"
+  puts "   | X |   "
+  puts "-----------"
+  puts "   |   |   "
+
+  print board = [" O"," "," "," "," "," "," "," "," "]
+
+  puts " O |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+
+   print board = [" X","X","X"," "," "," "," "," "," "]
+
+  puts " X | X | X "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+
+ print board = [" "," "," "," "," "," ","O","O","O"]
+
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts " O | O | O "
+
+   print board = ["X"," "," ","X"," "," "," "," ","X"]
+
+  puts " X |   |   "
+  puts "-----------"
+  puts "   | X |   "
+  puts "-----------"
+  puts "   |   | X "
+
+   print board = [" "," ","O"," ","0"," ","O"," "," "]
+
+  puts "   |   | O "
+  puts "-----------"
+  puts "   | O |   "
+  puts "-----------"
+  puts " O |   |   "
+
+    print board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
+
+  puts " X | X | X "
+  puts "-----------"
+  puts " X | O | O "
+  puts "-----------"
+  puts " X | O | O "
+
+board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+
+  puts " X | O | X "
+  puts "-----------"
+  puts " O | X | X "
+  puts "-----------"
+  puts " O | X | O "
+
+end
