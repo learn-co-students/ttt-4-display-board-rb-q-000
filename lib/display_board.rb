dff9c8fb57c936826b5946ed8a41a264d6a26185
chriswigington <chriswigington@gmail.com>
2016-04-03 14:50:33 -0400

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(xox)
+  line = "-----------"
+  puts " #{xox[0]} | #{xox[1]} | #{xox[2]} "
+  puts line
+  puts " #{xox[3]} | #{xox[4]} | #{xox[5]} "
+  puts line
+  puts " #{xox[6]} | #{xox[7]} | #{xox[8]} "
+end
