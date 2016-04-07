d74fb554664f9330ffd1e755008fb83549c2c232
M. Edgar Joel <mkakajoel@gmail.com>
2016-04-07 23:00:28 +0000

    Finished display board lab

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board(board)
+  row1 = " #{board[0]} | #{board[1]} | #{board[2]} \n"
+  row2 = " #{board[3]} | #{board[4]} | #{board[5]} \n"
+  row3 = " #{board[6]} | #{board[7]} | #{board[8]} \n"
+  column = "-----------\n"
+  puts "#{row1}#{column}#{row2}#{column}#{row3}"
+end
