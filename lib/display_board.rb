8e4e3fc2da8d3bded1efa754b54daa941bfac474
LizSmall <liz_too@hotmail.com>
2016-04-11 21:18:26 +0000

    nitrous-backup at Mon Apr 11 21:18:26 UTC 2016

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,77 +1,89 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-board = [" "," "," "," "," "," "," "," "," "]
+#board = [" "," "," "," "," "," "," "," "," "]
+
+#  puts "   |   |   "
+ # puts "-----------"
+ # puts "   |   |   "
+#  puts "-----------"
+#  puts "   |   |   "
+
+#print board = [" "," "," ","X"," "," "," "," "," "]
+
+#  puts "   |   |   "
+#  puts "-----------"
+#  puts "   | X |   "
+#  puts "-----------"
+#  puts "   |   |   "
+
+ # print board = [" O"," "," "," "," "," "," "," "," "]
+
+ # puts " O |   |   "
+ # puts "-----------"
+ # puts "   |   |   "
+ # puts "-----------"
+#  puts "   |   |   "
+
+ #  print board = [" X","X","X"," "," "," "," "," "," "]
+
+ # puts " X | X | X "
+ # puts "-----------"
+ # puts "   |   |   "
+#  puts "-----------"
+#  puts "   |   |   "
+
+# print board = [" "," "," "," "," "," ","O","O","O"]
+
+ # puts "   |   |   "
+#  puts "-----------"
+#  puts "   |   |   "
+#  puts "-----------"
+#  puts " O | O | O "
+
+#   print board = ["X"," "," ","X"," "," "," "," ","X"]
+
+ # puts " X |   |   "
+ # puts "-----------"
+ # puts "   | X |   "
+#  puts "-----------"
+#  puts "   |   | X "
+
+#   print board = [" "," ","O"," ","0"," ","O"," "," "]
+
+#  puts "   |   | O "
+#  puts "-----------"
+#  puts "   | O |   "
+#  puts "-----------"
+#  puts " O |   |   "
+
+ #   print board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
+
+#  puts " X | X | X "
+#  puts "-----------"
+#  puts " X | O | O "
+#  puts "-----------"
+  #puts " X | O | O "
+
+#board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+
+  #puts " X | O | X "
+ # puts "-----------"
+ # puts " O | X | X "
+ # puts "-----------"
+ # puts " O | X | O "
+
+#end
 
 def display_board(board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+  spaces= "   |   |   "
+  lines= "-----------"
 
-print board = [" "," "," ","X"," "," "," "," "," "]
-
-  puts "   |   |   "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   | X |   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "   |   |   "
-
-  print board = [" O"," "," "," "," "," "," "," "," "]
-
-  puts " O |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-
-   print board = [" X","X","X"," "," "," "," "," "," "]
-
-  puts " X | X | X "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-
- print board = [" "," "," "," "," "," ","O","O","O"]
-
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts " O | O | O "
-
-   print board = ["X"," "," ","X"," "," "," "," ","X"]
-
-  puts " X |   |   "
-  puts "-----------"
-  puts "   | X |   "
-  puts "-----------"
-  puts "   |   | X "
-
-   print board = [" "," ","O"," ","0"," ","O"," "," "]
-
-  puts "   |   | O "
-  puts "-----------"
-  puts "   | O |   "
-  puts "-----------"
-  puts " O |   |   "
-
-    print board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
-
-  puts " X | X | X "
-  puts "-----------"
-  puts " X | O | O "
-  puts "-----------"
-  puts " X | O | O "
-
-board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-
-  puts " X | O | X "
-  puts "-----------"
-  puts " O | X | X "
-  puts "-----------"
-  puts " O | X | O "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 
 end
+
