548257a587d7e567f3a32173b7c78a56827c017f
David O'Dea <d.odea4@nuigalway.ie>
2016-04-12 21:57:08 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = [" ","O","X"," "," ","O","X","O"," "]
+board = [" ","O","X"," "," ","O","X","O"," "]       #declare the board array as described in the rspec
 
-def display_board(board)
+def display_board(board)                            #defining the board method to take the board array as argument
 puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
