1aa3642c9b99a237ae0a40bc49eeac53bdbfa447
David O'Dea <d.odea4@nuigalway.ie>
2016-05-18 20:46:08 +0000

    nitrous-backup at Wed May 18 20:46:08 UTC 2016

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,7 @@
-# Define display_board that accepts a board and prints
-# out the current state.
-board = [" ","O","X"," "," ","O","X","O"," "]       #declare the board array as described in the rspec
+#display_board
+board = [" "," "," "," "," "," "," "," "," "]
 
-def display_board(board)                            #defining the board method to take the board array as argument
+def display_board(board)
 puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -10,5 +9,12 @@ puts "-----------"
 puts " #{board[6]} | #{board[7]} | #{board[8]} "
 
 end
-
 display_board(board)
+
+#valid_move
+def valid_move?(board, position)
+  position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
+end
+def position_taken?(board, location)
+   board[location] != " "
+ end
