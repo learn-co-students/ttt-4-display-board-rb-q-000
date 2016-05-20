7832c281be55d95b8fc960b71e9071da3e0f368d
tushar patil <tushar.sql@gmail.com>
2016-05-20 15:44:51 -0400

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board (arg)
+
+print " #{arg[0]} | #{arg[1]} | #{arg[2]}
+-----------
+ #{arg[3]} | #{arg[4]} | #{arg[5]}
+-----------
+ #{arg[6]} | #{arg[7]} | #{arg[8]} "
+end
+
+board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+display_board(board)
