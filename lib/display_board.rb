0ec86f587afe33633b4ab6e59979966c7a4c6964
lydie  toussaint <lydietoussaint123@gmail.com>
2016-06-04 10:57:10 -0400

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,102 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board= [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+
+puts   "   |   |   "
+puts   "-----------"
+puts   "   |   |   "
+puts   "-----------"
+puts   "   |   |   "
+
+
+ def display_board (board= [" ", " ", " ", " ", " X ", " ", " ", " ", " "])
+
+puts   "   |   |   "
+puts   "-----------"
+puts   "   | X |   "
+puts   "-----------"
+puts   "   |   |   "
+
+def display_board (board= [" 0 ", " ", " ", " ", "  ", " ", " ", " ", " "])
+
+puts   " O |   |   "
+puts   "-----------"
+puts   "   |   |   "
+puts   "-----------"
+puts   "   |   |   "
+
+def display_board (board= ["O", " ", " ", " ", " X ", " ", " ", " ", " "])
+
+puts   " O |   |   "
+puts   "-----------"
+puts   "   | X |   "
+puts   "-----------"
+puts   "   |   |   "
+
+def display_board (board= ["X", "X", "X", " ", "  ", " ", " ", " ", " "])
+
+puts   " X | X | X "
+puts   "-----------"
+puts   "   |   |   "
+puts   "-----------"
+puts   "   |   |   "
+
+def display_board (board= [" ", " ", " ", " ", "  ", " ", "O", "O", "O"])
+
+puts   "   |   |   "
+puts   "-----------"
+puts   "   |   |   "
+puts   "-----------"
+puts   " O | O | O "
+
+def display_board (board= ["X", " ", " ", " ", "X", " ", " ", " ", "X"])
+
+puts   " X |   |   "
+puts   "-----------"
+puts   "   | X |   "
+puts   "-----------"
+puts   "   |   | X "
+
+def display_board (board= ["O", " ", " ", " ", " O ", " ", " ", " ", "0"])
+
+puts   "   |   | O "
+puts   "-----------"
+puts   "   | O |   "
+puts   "-----------"
+puts   " O |   |   "
+
+def display_board (board= ["X", "X", "X", "X", "O", "O", "O", "X", "O"])
+
+puts   " X | X | X "
+puts   "-----------"
+puts   " X | O | O "
+puts   "-----------"
+puts   " X | O | O "
+
+def display_board (board= ["X", "O", "X", "X", "O", "O", "X", "O", "O"])
+
+puts   " X | O | X "
+puts   "-----------"
+puts   " O | X | X "
+puts   "-----------"
+puts   " O | X | O "
+
+def display_board (board= ["X", "X", "X", "O", "X", "X", "O", "X", "O"])
+
+puts   " X | X | X "
+puts   "-----------"
+puts   " O | X | X "
+puts   "-----------"
+puts   " O | X | O "
+
+end
+end
+end
+end
+end
+end
+end
+end
+end
+end
+end
