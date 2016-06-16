a5101b9cf2e291238ba681e82cfc8ba7a93aeee8
paige11 <ellis.paige.v@gmail.com>
2016-06-16 14:46:02 -0400

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,22 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+  line = "-----------"
+  empty = "   "
+  if board.length < 9
+    b = 9 - board.length
+    b.times do
+      board << " "
+    end
+  end
+  board.each_with_index do |c, i|
+    if c == "X" || c == "O"
+      board[i] = " #{c} "
+    else
+      board[i] = empty
+    end
+  end
+  puts "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts line
+  puts "#{board[3]}|#{board[4]}|#{board[5]}"
+  puts line
+  puts "#{board[6]}|#{board[7]}|#{board[8]}"
+end
