50d56b9e6f22849265a7ec4df67cc92fa0a823d1
cyanocobalamin <orenjuice@yahoo.com>
2016-06-20 19:10:02 -0400

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,17 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board (cell)
+  print "\n"
+  puts " #{cell[0]} " + "| #{cell[1]} |" + " #{cell[2]} " , 
+       '-' + '-' + '-' + '-' + '-' + '-' + '-' + '-' + '-' + '-' + '-' ,
+       " #{cell[3]} " + "| #{cell[4]} |" + " #{cell[5]} " ,
+       '-' + '-' + '-'+ '-' + '-' + '-' + '-' + '-' + '-' + '-' + '-' ,
+       " #{cell[6]} " + "| #{cell[7]} |" + " #{cell[8]} "
+  print "\n"
+end    
+
+board = [" ", " ", " "," ", " ", " "," ", " ", " "]
+
+display_board (board)
+
+
+
+  
