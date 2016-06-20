098568d9525cc338a8b86807cac2be7b3ddb4724
paige11 <ellis.paige.v@gmail.com>
2016-06-20 02:00:10 -0400

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,22 +1,7 @@
-def display_board(board)
-  line = "-----------"
-  empty = "   "
-  if board.length < 9
-    b = 9 - board.length
-    b.times do
-      board << " "
-    end
-  end
-  board.each_with_index do |c, i|
-    if c == "X" || c == "O"
-      board[i] = " #{c} "
-    else
-      board[i] = empty
-    end
-  end
-  puts "#{board[0]}|#{board[1]}|#{board[2]}"
-  puts line
-  puts "#{board[3]}|#{board[4]}|#{board[5]}"
-  puts line
-  puts "#{board[6]}|#{board[7]}|#{board[8]}"
+def display_board (board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "  
 end
