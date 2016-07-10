a884ec5a1eff92343dbcddc4c12489039bfa626b
Matthew Vasseur <matthew.vasseur@yale.edu>
2016-07-10 21:32:15 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 # out the current state.
 def display_board(b)
     row_break = "-"*11
-    puts " " + b[0] + " | " + b[1] + " | " + b[2] + " "
+    puts " #{b[0]} | #{b[1]} | #{b[2]} "
     puts row_break
-    puts " " + b[3] + " | " + b[4] + " | " + b[5] + " "
+    puts " #{b[3]} | #{b[4]} | #{b[5]} "
     puts row_break
-    puts " " + b[6] + " | " + b[7] + " | " + b[8] + " "
+    puts " #{b[6]} | #{b[7]} | #{b[8]} "
 end
