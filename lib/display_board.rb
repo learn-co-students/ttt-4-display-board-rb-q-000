db2e8db6ba0d17c75315cba9720d97a11bba1dfd
tnegoshi <gbardhoshi@gmail.com>
2016-07-17 01:19:07 -0400

    update puts to print

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
 def display_board(board)
-  puts " #{board[0]} " + "|" + " #{board[1]} " + "|" + " #{board[2]} " + "\n" + "-----------" + "\n" +
+  print " #{board[0]} " + "|" + " #{board[1]} " + "|" + " #{board[2]} " + "\n" + "-----------" + "\n" +
   " #{board[3]} " + "|" + " #{board[4]} " + "|" + " #{board[5]} " + "\n" + "-----------" + "\n" +
   " #{board[6]} " + "|" + " #{board[7]} " + "|" + " #{board[8]} "
 end
+
+# board = [" "," "," "," "," "," "," "," "," "]
+# display_board(board)
