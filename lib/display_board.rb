68565029e5bdeb67f7f47094137bbfc08bb8c02f
Kyahan Flynch <kyahanflynch@gmail.com>
2016-07-26 09:38:10 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,17 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = ["X","O","X","O","O","X","O","X","X"]
+
+def display_board(xo)
+  dash = "-----------"
+  line1 = " #{xo[0]} | #{xo[1]} | #{xo[2]} "
+  line2 = " #{xo[3]} | #{xo[4]} | #{xo[5]} "
+  line3 = " #{xo[6]} | #{xo[7]} | #{xo[8]} "
+  puts line1
+  puts dash
+  puts line2
+  puts dash
+  puts line3
+end
+
+display_board(board)
