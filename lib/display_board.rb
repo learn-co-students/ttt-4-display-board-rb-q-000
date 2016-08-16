1530dea1ee5d54807107e6424dbbcd0f2023a78e
Genie Nyiramana <nigenie5@yahoo.fr>
2016-08-16 00:32:34 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,23 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+  divider = "-----------"
+  slash = "|"
+  print board[0]
+  print slash
+  print board[1]
+  print slash
+  puts board[2]
+  puts divider
+  print board[3]
+  print slash
+  print board[4]
+  print slash
+  puts board[5]
+  puts divider
+  print board[6]
+  print slash
+  print board[7]
+  print slash
+  print board[8]
+end
+board = [" O ", "    ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
+display_board(board)
