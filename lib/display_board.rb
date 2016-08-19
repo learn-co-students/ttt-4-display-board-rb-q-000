90e5c94728d25938bae4d138cbbc8556099db636
Sofia Martinez <ms.sofia333@gmail.com>
2016-08-19 21:16:32 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 def display_board(board)
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   dotted_line = "-----------"
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts dotted_line
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts dotted_line
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts dotted_line
 end
